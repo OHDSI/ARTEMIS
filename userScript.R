3a6159450af2d8080dc00f459326c0134277f1be
Svetozar Nesic <snesic@gmail.com>
2025-11-24 12:36:55 +0100

    update test script

--- userScript.R
+++ userScript.R
@@ -1,71 +1,91 @@
-#
-Sys.setenv(RETICULATE_PYTHON = "/Users/snesic/miniconda3/envs/tsw-env/bin/python3")
+# Example script that walks through running ARTEMIS, 
+# using the package’s included dummy database as input
 
-devtools::load_all("../ARTEMIS/")
+# If you are using a custom Python environment, 
+# specify the full path to the desired Python executable:
+# Sys.setenv(RETICULATE_PYTHON = "/full/path/to/python3")
+library(ARTEMIS)
 
-##### INPUT #####
-# TODO: check if we want to keep this as an example:
-connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "redshift",
-                                                                server="server/database",
-                                                                user="username",
-                                                                port = "9999",
-                                                                password="password",
-                                                                pathToDriver = "./JBDC")
+##### Prepare input #####
+# Example: create connection details for a Redshift database using DatabaseConnector
+# connectionDetails <- DatabaseConnector::createConnectionDetails(
+#     dbms = "redshift",
+#     server = "server/database",
+#     user = "username",
+#     port = "9999",
+#     password = "password",
+#     pathToDriver = "./JBDC"
+# )
 
 # Load SQLite test database within the package
 db_path <- system.file("extdata", "testing_db.sqlite", package = "ARTEMIS")
 
-connectionDetails <- DatabaseConnector::createConnectionDetails(
-    dbms = "sqlite",
-    server = db_path
-)
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "sqlite", server = db_path)
 
 
 df_json <- loadCohort()
 name <- "lungcancer"
 
 validdrugs <- loadDrugs()
-regimens <- loadRegimens()
+regimens <- loadRegimens(condition = "all")
 regGroups <- loadGroups()
 
-cdmSchema      <- "main"
-writeSchema    <- "main"
+cdmSchema <- "main"
+writeSchema <- "main"
 
-##### MAIN #####
-con_df <- getConDF(connectionDetails = connectionDetails, 
-                   json = df_json$json, 
-                   name = name, 
-                   cdmSchema = cdmSchema, 
-                   writeSchema = writeSchema)
+##### Fetch data #####
+con_df <- getConDF(
+    connectionDetails = connectionDetails,
+    json = df_json$json[1],
+    name = name,
+    cdmSchema = cdmSchema,
+    writeSchema = writeSchema
+)
 
 
-# Check if the dates are correctly written, and if not: 
+# Check if the dates are correctly written
 con_df$drug_exposure_start_date
-con_df$drug_exposure_start_date <- as.POSIXct(con_df$drug_exposure_start_date, 
-                                              origin = "1970-01-01", tz = "UTC")
+# and if not:
+con_df$drug_exposure_start_date <- as.POSIXct(con_df$drug_exposure_start_date,
+                                              origin = "1970-01-01",
+                                              tz = "UTC")
 
-stringDF <- stringDF_from_cdm(con_df = con_df, writeOut = F, validDrugs = validdrugs)
+# Prepare a data.frame of patient drug records used in the alignment step
+stringDF <- stringDF_from_cdm(con_df = con_df,
+                              writeOut = F,
+                              validDrugs = validdrugs)
 
 ## Alignment
 
-output_all <- stringDF %>% generateRawAlignments(regimens = regimens,
-                                                 g = 0.4,
-                                                 Tfac = 0.4,
-                                                 method = "PropDiff",
-                                                 verbose = 0)
+output_all <- stringDF %>%
+    generateRawAlignments(
+        regimens = regimens,
+        g = 0.4,
+        Tfac = 0.4,
+        method = "PropDiff",
+        verbose = 0
+    )
 
 ## Post-process Alignment
 
-processedAll <- output_all %>% 
-    processAlignments(regimenCombine = 28, regimens = regimens)
+processedAll <- output_all %>%
+    processAlignments(regimens = regimens, regimenCombine = 28)
 
 processedEras <- processedAll %>% calculateEras()
 
-# Regimen stats does not work 
+# Regimen stats does not work
 # regStats <- processedEras %>% generateRegimenStats()
 
-##### OUTPUT #####
+##### Save all outputs #####
 
-writeOutputs(output_all, processedAll = processedAll, processedEras = processedEras,
-             connectionDetails = connectionDetails, cdmSchema = cdmSchema, regGroups = regGroups,
-             regStats = regStats, stringDF = stringDF, con_df = con_df)
+writeOutputs(
+    output_all,
+    processedAll = processedAll,
+    processedEras = processedEras,
+    connectionDetails = connectionDetails,
+    cdmSchema = cdmSchema,
+    regGroups = regGroups,
+    regStats = regStats,
+    stringDF = stringDF,
+    con_df = con_df
+)
